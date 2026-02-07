df00e8b1037598ef65005877871cfca164152c77
somebody else <ethanfrancis577@gmail.com>
2026-02-07 15:19:19 -0500

    base framework done :shrug:

---
+++ src/lib/components/TextInput.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    type ControllerPrompt = 
+    "buttona" | "buttonb" | "buttonx" | "buttony"
+    | "dpadd" | "dpadu" | "dpadl" | "dpadr"
+    | "lstick" | "rstick"
+    | "lb" | "lt" | "rb" | "rt"
+    | "menu" | "share" | "view"
+    
+
+    let {tgt}: {tgt: ControllerPrompt} = $props();
+    
+</script>
+
+<div class="flex flex-row">
+    <enhanced:img class="float-left w-10 invert" alt="{tgt} image" about="{tgt} image" src={`/src/lib/assets/controllers/xbox/${tgt}.png`}/>
+    <input class="ui_input">
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,34 @@
-<script></script>
+<script lang="ts">
+    import xbox_controller from '$lib/assets/xbox_controller.svg'
+    import TextInput from '$lib/components/TextInput.svelte';
+    
+    let happy = $state("happy")
+    import { onMount } from 'svelte';
+    onMount(createLine)
 
-<h1>Welcome to SvelteKit</h1>
-<p class="text-red-500">Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+    function createLine() {
+        
+    }
+</script> 
+
+
+<div class="w-screen h-screen flex">
+    <div class="flex flex-row">
+        <div class="flex flex-col w-[33vw] justify-center">
+            
+        </div>
+        <div class="flex flex-col w-[33vw] justify-center">
+            <img id="xboxc" alt="xbox controller" src={xbox_controller} class="w-75 lg:w-200">
+        </div>
+        <div class="flex flex-col w-[33vw] justify-center">
+            <TextInput tgt="rb"></TextInput>
+            <TextInput tgt="rt"></TextInput>
+            <TextInput tgt="buttona"></TextInput>
+            <TextInput tgt="buttonb"></TextInput>
+            <TextInput tgt="buttonx"></TextInput>
+            <TextInput tgt="buttony"></TextInput>
+            <TextInput tgt="menu"></TextInput>
+            <TextInput tgt="rstick"></TextInput>
+        </div>
+    </div>
+</div>
